5fb49daeb38f64ca3cf29d43f19c57c30c04af58
estebanGarnil <esteb.garnil@gmail.com>
2026-05-17 23:01:07 +0200

    Add export in core/init file

--- boilr_generator/core/__init__.py
+++ boilr_generator/core/__init__.py
@@ -1,8 +1,39 @@
 from boilr_generator.core.module import ResolvedModule
 from boilr_generator.core.project import ResolvedProject
+from boilr_generator.core.exceptions import (
+    BoilrError,
+    DuplicateModuleError,
+    GenerationError,
+    InvalidModuleDefinitionError,
+    ManifestError,
+    ManifestParsingError,
+    ModuleCompatibilityError,
+    ModuleNotFoundError,
+    ModuleRequirementError,
+    ModuleVariableError,
+    OutputDirectoryError,
+    RegistryError,
+    ResolverError,
+    TemplateRenderError,
+)
 
 __all__ =  [
     "ResolvedModule",
-    "ResolvedProject"
+    "ResolvedProject",
+    "BoilrError",
+    "DuplicateModuleError",
+    "GenerationError",
+    "InvalidModuleDefinitionError",
+    "ManifestError",
+    "ManifestParsingError",
+    "ModuleCompatibilityError",
+    "ModuleNotFoundError",
+    "ModuleRequirementError",
+    "ModuleVariableError",
+    "OutputDirectoryError",
+    "RegistryError",
+    "ResolverError",
+    "TemplateRenderError",
 ]
+
 
